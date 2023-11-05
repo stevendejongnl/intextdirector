fec7d6327192723d8f19a5304afdb5088ac81639
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2023-11-05 20:01:17 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -7,88 +7,78 @@ type RedirectOptions = {
 
 function showDebugMode(): void {
   const debugElement = document.querySelector('.debug')
-
   debugElement.textContent = 'Debug mode active'
 }
 
 function showError(message: string): void {
   const errorElement = document.querySelector('.error')
-
   errorElement.textContent = message
 }
 
-function updateTimeoutCounter(milliseconds) {
+function updateTimeoutCounter(milliseconds: number): void {
   const timeoutElement = document.querySelector('.timeout')
   const seconds = Math.ceil(milliseconds / 1000)
   timeoutElement.textContent = `Checking internal access: ${seconds} seconds`
 }
 
-function startTimeoutCounter(options: RedirectOptions) {
-  const controller = new AbortController()
-  const timeoutId = setTimeout(() => controller.abort(), options.timeout)
-
-  const timeoutElement = document.querySelector('.timeout')
-  const startTime = Date.now()
-  const interval = setInterval(() => {
-    const elapsedTime = Date.now() - startTime
-    const remainingTime = options.timeout - elapsedTime
-    if (remainingTime <= 0) {
-      clearInterval(interval)
-      timeoutElement.remove()
-    } else {
-      updateTimeoutCounter(remainingTime)
-    }
-  }, 1000)
-
-  fetch(options.internalURL, {
-    method: 'HEAD',
-    signal: controller.signal
-  })
-    .then(response => {
-      clearTimeout(timeoutId)
-      clearInterval(interval)
-
-      if (response.ok) {
-        if (options.debug) {
-          console.log(`Response for internal is okay, redirect should go to ${options.internalURL}`)
-
-          timeoutElement.remove()
-          return
-        }
-
-        window.location.href = options.internalURL
-      } else {
-        if (options.debug) {
-          console.log(`Response for internal is not okay, redirect should go to ${options.externalURL}`)
-
-          timeoutElement.remove()
-          return
-        }
-
-        window.location.href = options.externalURL
-      }
-    })
-    .catch(() => {
-      clearTimeout(timeoutId)
-      clearInterval(interval)
-
-      if (options.debug) {
-        console.log(`Error caught, redirect should go to ${options.externalURL}`)
-
-        timeoutElement.remove()
-        return
-      }
-
-      window.location.href = options.externalURL
-    })
+async function checkInternalAccess(url: string): Promise<boolean> {
+  try {
+    const response = await fetch(url, { method: 'HEAD' })
+    return response.ok
+  } catch (error) {
+    return false
+  }
 }
 
-function initRedirect() {
+async function startTimeoutCounter(options: RedirectOptions): Promise<void> {
+  const timeoutElement = document.querySelector('.timeout')
+  const startTime = Date.now()
+
+  try {
+    const isInternalAccessible = await checkInternalAccess(options.internalURL)
+    const elapsedTime = Date.now() - startTime
+    const remainingTime = options.timeout - elapsedTime
+
+    if (isInternalAccessible) {
+      if (options.debug) {
+        console.log(`Internal access is okay, redirecting to ${options.internalURL}`)
+        timeoutElement.remove()
+      }
+
+      if (remainingTime > 0) {
+        setTimeout(() => {
+          window.location.href = options.internalURL
+        }, remainingTime)
+      } else {
+        window.location.href = options.internalURL
+      }
+    } else {
+      if (options.debug) {
+        console.log(`Internal access is not okay, redirecting to ${options.externalURL}`)
+        timeoutElement.remove()
+      }
+
+      if (remainingTime > 0) {
+        setTimeout(() => {
+          window.location.href = options.externalURL
+        }, remainingTime)
+      } else {
+        window.location.href = options.externalURL
+      }
+    }
+  } catch (error) {
+    console.error(`Error caught: ${error.message}`)
+    timeoutElement.remove()
+    window.location.href = options.externalURL
+  }
+}
+
+function initRedirect(): void {
   const urlParams = new URLSearchParams(window.location.search)
   const internalURL = urlParams.get('intern')
   const externalURL = urlParams.get('extern')
   const debug = urlParams.get('debug') === 'true' || false
-  const timeout = parseInt(urlParams.get('timeout') || '15000')
+  const timeout = parseInt(urlParams.get('timeout') || '15000', 10)
 
   if (debug) {
     showDebugMode()
@@ -96,7 +86,7 @@ function initRedirect() {
 
   if (!internalURL || !externalURL) {
     showError('Internal and external URLs are required.')
-    return
+    return;
   }
 
   updateTimeoutCounter(timeout)
